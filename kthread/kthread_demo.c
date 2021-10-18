6154fe7e2223d6188832f14463bb0cf4b6f47a26
yuchao <yuchao9225@163.com>
2021-10-18 20:54:51 +0800

    ...

--- kthread/kthread_demo.c
+++ kthread/kthread_demo.c
@@ -1,14 +1,22 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/kthread.h>
+#include <linux/delay.h>
 
 static struct task_struct *demo_task;
 
 static int do_task(void *data)
 {
 	while (1) {
-		/* TODO: 内核中的延时操作 */
+		msleep(10);
+
 		printk("do something...\n");
+
+		/*
+		 * 没有stop，会导致kthread_stop()不返回，为什么？
+		 */
+		if (kthread_should_stop())
+			break;
 	}
 
 	return 0;
@@ -20,11 +28,12 @@ static int __init demo_init(void)
 
 	demo_task = kthread_create(do_task, NULL, "%s", "demo_task");
 	if (IS_ERR(demo_task)) {
-		ret = ERR_PTR(demo_task);
+		ret = PTR_ERR(demo_task);
 		demo_task = NULL;
 		return ret;
 	}
-	/* TODO: 启动之后必须要执行唤醒操作么？ */
+
+	/* TODO: 代码分析为何必须要执行唤醒操作？ */
 	wake_up_process(demo_task);
 
 	return 0;
--- rcu/rcu_demo.c
+++ rcu/rcu_demo.c
@@ -1,37 +1,114 @@
 /*
  * 模拟net_device{} 中 ip_ptr 指针的使用，研究RCU 的作用.
+ *
+ * rcu_assign_pointer()
+ * rcu_dereference()
+ * rcu_read_lock()
+ * rcu_read_unlock()
+ *
+ * 编译通过内核列表:
+ * 2.6.35.6
  */
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/delay.h>
+#include <linux/kthread.h>
+#include <linux/rcupdate.h>
+#include <linux/slab.h>
 
-static int *ptr;
-static int value = 10;
+struct TEST {
+	struct rcu_head	rcu_head;
+};
+struct TEST *ptest;
 
-/*
- * 线程1 中
- * 加RCU 锁，获取ptr 指针，调度出去一段时间之后，访问数值
- */
+static struct task_struct *task1;
+static struct task_struct *task2;
 
-/*
- * 线程2 中
- * 首先调度出去一段时间，设置ptr 为空
- */
+static int do_task1(void *data)
+{
+	rcu_read_lock();
+#if 0
+	rcu_dereference(ptest);
+#endif
+	/* TODO: 为什么这里用msleep() 的时候这里不行 */
+	mdelay(10000);
+	printk("do task1 after sleep\n");
+	rcu_read_unlock();
+
+	while (1) {
+		msleep(10);
+		if (kthread_should_stop())
+			break;
+	}
+	return 0;
+}
+
+static void rcu_test_print(struct rcu_head *head)
+{
+	printk("rcu test print\n");
+}
+
+static int do_task2(void *data)
+{
+	mdelay(1000);
+
+	ptest = NULL;
+	call_rcu(&ptest->rcu_head, rcu_test_print);
+
+	while (1) {
+		msleep(10);
+		if (kthread_should_stop())
+			break;
+	}
+	return 0;
+}
 
 static int __init demo_init(void)
 {
-	/* 设置ptr */
-	rcu_assign_pointer(ptr, &value);
+	int ret;
+	/* TODO: 检测内存是否申请成功 */
+	ptest = kzalloc(sizeof(*ptest), GFP_KERNEL);
+#if 0
+	rcu_assign_pointer(ptest, ptest);
+#endif
 
 	/* 开启两个内核线程 */
+	task1 = kthread_create(do_task1, NULL, "demo_task1");
+	if (IS_ERR(task1)) {
+		printk("kthread err 1\n");
+		ret = PTR_ERR(task1);
+		task1 = NULL;
+		return ret;
+	}
+	printk("create task 1 success\n");
+	task2 = kthread_create(do_task2, NULL, "demo_task2");
+	if (IS_ERR(task2)) {
+		printk("kthread err 2\n");
+		ret = PTR_ERR(task2);
+		task2 = NULL;
+		goto err;
+	}
+	printk("create task 2 success\n");
 
-
+	wake_up_process(task1);
+	wake_up_process(task2);
 
 	return 0;
+err:
+	kthread_stop(task1);
+	return ret;
 }
       
 static void __exit demo_exit(void)
 {
+	/* 内存释放 */
+	kfree(ptest);
+
 	/* 结束两个内核线程 */
+	if (task1)
+		kthread_stop(task1);
+	if (task2)
+		kthread_stop(task2);
 }
 
 module_init(demo_init);
