06992cab7905c639810d8de73c5b204c8395c9ad
rivsidn <rivsidn@163.com>
2021-10-19 20:51:26 +0800

    ...

--- rcu/rcu_demo.c
+++ rcu/rcu_demo.c
@@ -1,10 +1,10 @@
 /*
  * 模拟net_device{} 中 ip_ptr 指针的使用，研究RCU 的作用.
  *
- * rcu_assign_pointer()
- * rcu_dereference()
- * rcu_read_lock()
- * rcu_read_unlock()
+ * do_task1 首先获取指针，进入到临近区中；
+ * do_task2 设置指针为空，设置 call_rcu() 钩子函数；
+ * do_task1 在临界区内可以继续操作该部分内存，当退出临界区之后，call_rcu() 中
+ * 设置的钩子函数会被调用，释放内存.
  *
  * 编译通过内核列表:
  * 2.6.35.6
@@ -21,18 +21,17 @@ struct TEST {
 };
 struct TEST *ptest;
 
+struct timeval tv;
+
 static struct task_struct *task1;
 static struct task_struct *task2;
 
+/* rcu_read_lock()/rcu_read_unlock() 中不能休眠 */
 static int do_task1(void *data)
 {
 	rcu_read_lock();
-#if 0
 	rcu_dereference(ptest);
-#endif
-	/* TODO: 为什么这里用msleep() 的时候这里不行 */
-	mdelay(10000);
-	printk("do task1 after sleep\n");
+	mdelay(10000);		//忙等待10s
 	rcu_read_unlock();
 
 	while (1) {
@@ -43,17 +42,24 @@ static int do_task1(void *data)
 	return 0;
 }
 
-static void rcu_test_print(struct rcu_head *head)
+static void rcu_demo_free(struct rcu_head *head)
 {
-	printk("rcu test print\n");
+	struct TEST *tmp = container_of(head, struct TEST, rcu_head);
+
+	do_gettimeofday(&tv);
+	printk("%ld %ld:free ptest\n", tv.tv_sec, tv.tv_usec);
+	kfree(tmp);
 }
 
 static int do_task2(void *data)
 {
+	struct TEST *tmp = ptest;
+
+	/* 忙等待1s */
 	mdelay(1000);
 
-	ptest = NULL;
-	call_rcu(&ptest->rcu_head, rcu_test_print);
+	rcu_assign_pointer(ptest, NULL);
+	call_rcu(&tmp->rcu_head, rcu_demo_free);
 
 	while (1) {
 		msleep(10);
@@ -66,11 +72,13 @@ static int do_task2(void *data)
 static int __init demo_init(void)
 {
 	int ret;
-	/* TODO: 检测内存是否申请成功 */
+
+	ret = -ENOMEM;
 	ptest = kzalloc(sizeof(*ptest), GFP_KERNEL);
-#if 0
+	if (!ptest) {
+		goto out;
+	}
 	rcu_assign_pointer(ptest, ptest);
-#endif
 
 	/* 开启两个内核线程 */
 	task1 = kthread_create(do_task1, NULL, "demo_task1");
@@ -78,31 +86,37 @@ static int __init demo_init(void)
 		printk("kthread err 1\n");
 		ret = PTR_ERR(task1);
 		task1 = NULL;
-		return ret;
+		goto err0;
 	}
-	printk("create task 1 success\n");
+	do_gettimeofday(&tv);
+	printk("%ld %ld:create task 1 success\n", tv.tv_sec, tv.tv_usec);
 	task2 = kthread_create(do_task2, NULL, "demo_task2");
 	if (IS_ERR(task2)) {
 		printk("kthread err 2\n");
 		ret = PTR_ERR(task2);
 		task2 = NULL;
-		goto err;
+		goto err1;
 	}
-	printk("create task 2 success\n");
+	do_gettimeofday(&tv);
+	printk("%ld %ld:create task 2 success\n", tv.tv_sec, tv.tv_usec);
 
 	wake_up_process(task1);
 	wake_up_process(task2);
 
 	return 0;
-err:
+err1:
 	kthread_stop(task1);
+err0:
+	kfree(ptest);
+out:
 	return ret;
 }
       
 static void __exit demo_exit(void)
 {
 	/* 内存释放 */
-	kfree(ptest);
+	if (ptest)
+		kfree(ptest);
 
 	/* 结束两个内核线程 */
 	if (task1)
