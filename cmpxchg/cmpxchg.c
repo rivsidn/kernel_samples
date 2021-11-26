6b5e1c19dcdea9f986f037b7f8fdc5bdbc53c98a
rivsidn <rivsidn@163.com>
2021-11-26 18:08:57 +0800

    2021-11-26 18:08:57

--- cmpxchg/cmpxchg.c
+++ cmpxchg/cmpxchg.c
@@ -1,6 +1,23 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
+/*
+ * ret = cmpxchg(a, b, c);
+ *
+ * 执行逻辑可以这么理解:
+ * ret = a;
+ * if (a == b) {
+ *     a = c;
+ * } else {
+ *     ;
+ * }
+ *
+ * if (ret == b) {
+ *     说明执行过交换动作
+ * } else {
+ *     没执行过交换动作
+ * }
+ */
 static int __init demo_init(void)
 {
 	int ret;
@@ -14,9 +31,8 @@ static int __init demo_init(void)
 	ret = cmpxchg(&a, b, c);
 	printk("a %d b %d c %d ret %d\n", a, b ,c, ret);
 
-
 	/*
-	 * a == b 时，a 的值变成 c，b,c 值不变，返回值为 a 之前的值
+	 * a == b 时，a 的值变成 c，b,c 值不变，返回最初 a 的值
 	 */
 	a = 2; b = 2; c = 3;
 	printk("a %d b %d c %d ", a, b ,c);
